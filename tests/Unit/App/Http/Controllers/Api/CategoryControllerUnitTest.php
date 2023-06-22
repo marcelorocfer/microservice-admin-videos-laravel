e5f08d60283ac3f38b8299f4857fcd47eb067f0f
Marcelo <marceloengecomp@gmail.com>
2023-06-21 22:21:18 -0300

    Controller de listagem das categorias e TDD

---
+++ tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\App\Http\Controllers\Api;
+
+use Mockery;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\Api\CategoryController;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\DTO\Category\ListCategories\ListCategoriesOutputDTO;
+
+class CategoryControllerUnitTest extends TestCase
+{
+    public function testIndex()
+    {
+        $mockRequest = Mockery::mock(Request::class);
+        $mockRequest->shouldReceive('get')->andReturn('teste');
+
+        $mockOutputDTO = Mockery::mock(ListCategoriesOutputDTO::class, [
+            [], 1, 1, 1, 1, 1, 1, 1
+        ]);
+
+        $mockUseCase = Mockery::mock(ListCategoriesUseCase::class);
+        $mockUseCase->shouldReceive('execute')->andReturn($mockOutputDTO);
+
+        $controller = new CategoryController();
+        $response = $controller->index($mockRequest, $mockUseCase);
+
+        $this->assertIsObject($response->resource);
+        $this->assertArrayHasKey('meta', $response->additional);
+    }
+}
