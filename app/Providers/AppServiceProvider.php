334e6afdb94f427fd1d84878c163466b410eb152
Marcelo <marceloengecomp@gmail.com>
2023-06-26 11:22:25 -0300

    Teste e2e listar categorias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Eloquent\CategoryRepository;
+use Core\Domain\Repository\CategoryRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(
+            CategoryRepositoryInterface::class,
+            CategoryRepository::class
+        );
     }
 
     /**
---
+++ tests/Feature/Api/CategoryApiTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\Category;
+
+class CategoryApiTest extends TestCase
+{
+    protected $endpoint = '/api/categories';
+
+    public function test_list_empty_categories()
+    {
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_list_all_categories()
+    {
+        Category::factory()->count(30)->create();
+
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'last_page',
+                'current_page',
+                'first_page',
+                'per_page',
+                'to', 
+                'from'
+            ]
+        ]);
+    }
+
+    public function test_list_paginate_categories()
+    {
+        Category::factory()->count(30)->create();
+
+        $response = $this->getJson("$this->endpoint?page=2");
+
+        $response->assertStatus(200);
+        $this->assertEquals(2, $response['meta']['current_page']);
+        $this->assertEquals(30, $response['meta']['total']);
+    }
+}
