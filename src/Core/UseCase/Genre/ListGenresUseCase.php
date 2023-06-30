6323c01523e16c0a8c7177710d2a879892d2e4d0
marcelorocfer <marceloengecomp@gmail.com>
2023-06-30 10:24:02 -0300

    TDD usecase de listagem de gêneros com paginação

--- src/Core/UseCase/Genre/ListGenresUseCase.php
+++ src/Core/UseCase/Genre/ListGenresUseCase.php
@@ -3,7 +3,10 @@
 namespace Core\UseCase\Genre;
 
 use Core\Domain\Repository\GenreRepositoryInterface;
-use Core\UseCase\DTO\Genre\ListGenres\ListGenresInputDTO;
+use Core\UseCase\DTO\Genre\ListGenres\{
+    ListGenresInputDTO,
+    ListGenresOutputDTO,
+};
 
 class ListGenresUseCase
 {
@@ -14,8 +17,24 @@ class ListGenresUseCase
         $this->repository = $repository;
     }
 
-    public function execute(ListGenresInputDTO $input)
+    public function execute(ListGenresInputDTO $input): ListGenresOutputDTO
     {
-        
+        $response = $this->repository->paginate(
+            filter: $input->filter,
+            order: $input->order,
+            page: $input->page,
+            totalPage: $input->totalPage,
+        );
+
+        return new ListGenresOutputDTO(
+            items: $response->items(),
+            total: $response->total(),
+            last_page: $response->lastPage(),
+            first_page: $response->firstPage(),
+            current_page: $response->currentPage(),
+            per_page: $response->perPage(),
+            to: $response->to(),
+            from: $response->from(),
+        );
     }
 }
--- tests/Unit/UseCase/Genre/ListGenresUseCaseUnitTest.php
+++ tests/Unit/UseCase/Genre/ListGenresUseCaseUnitTest.php
@@ -6,22 +6,44 @@ use Mockery;
 use stdClass;
 use PHPUnit\Framework\TestCase;
 use Core\UseCase\Genre\ListGenresUseCase;
+use Core\Domain\Repository\PaginationInterface;
 use Core\Domain\Repository\GenreRepositoryInterface;
-use Core\UseCase\DTO\Genre\ListGenres\ListGenresInputDTO;
+use Core\UseCase\DTO\Genre\ListGenres\{ 
+    ListGenresInputDTO,
+    ListGenresOutputDTO
+};
 
 class ListGenresUseCaseUnitTest extends TestCase
 {
     public function test_usecase()
     {
         $mockRepository = Mockery::mock(stdClass::class, GenreRepositoryInterface::class);
+        $mockRepository->shouldReceive('paginate')->andReturn($this->mockPagination());
 
         $mockInputDTO = Mockery::mock(ListGenresInputDTO::class, [
             'test', 'desc', 1, 15
         ]);
 
         $useCase = new ListGenresUseCase($mockRepository);
-        $useCase->execute($mockInputDTO);
+        $response = $useCase->execute($mockInputDTO);
+
+        $this->assertInstanceOf(ListGenresOutputDTO::class, $response);
 
         Mockery::close();
+    }
+
+    protected function mockPagination(array $items = [])
+    {
+        $this->mockPagination = Mockery::mock(stdClass::class, PaginationInterface::class);
+        $this->mockPagination->shouldReceive('items')->andReturn($items);
+        $this->mockPagination->shouldReceive('total')->andReturn(0);
+        $this->mockPagination->shouldReceive('currentPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('firstPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('lastPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('perPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('to')->andReturn(0);
+        $this->mockPagination->shouldReceive('from')->andReturn(0);
+
+        return $this->mockPagination;
     }
 }
