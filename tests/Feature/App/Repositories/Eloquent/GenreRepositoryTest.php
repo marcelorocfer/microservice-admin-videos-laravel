fb334ffc3589355869f975b1e305d771efc0816d
marcelorocfer <marceloengecomp@gmail.com>
2023-07-06 10:31:52 -0300

    Testando a persistência do gênero

--- tests/Feature/App/Repositories/Eloquent/GenreRepositoryTest.php
+++ tests/Feature/App/Repositories/Eloquent/GenreRepositoryTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Models\Genre as Model;
 use Core\Domain\Entity\Genre as Entity;
 use App\Repositories\Eloquent\GenreRepository;
+use Core\Domain\Repository\GenreRepositoryInterface;
 
 class GenreRepositoryTest extends TestCase
 {
@@ -16,13 +17,34 @@ class GenreRepositoryTest extends TestCase
         parent::setUp();
         $this->repository = new GenreRepository(new Model());
     }  
+
+    public function testImplementsInterface()
+    {
+        $this->assertInstanceOf(GenreRepositoryInterface::class, $this->repository);
+    }
     
     public function testInsert()
     {
         $entity = new Entity(name: 'New genre');
-
         $response = $this->repository->insert($entity);
 
-        dump($response);
+        $this->assertEquals($entity->id, $response->id);
+        $this->assertEquals($entity->name, $response->name);
+        $this->assertDatabaseHas('genres', [
+            'id' => $entity->id()
+        ]);
+    }
+    
+    public function testInsertDeactivate()
+    {
+        $entity = new Entity(name: 'New genre');
+        $entity->deactivate();
+
+        $this->repository->insert($entity);
+        
+        $this->assertDatabaseHas('genres', [
+            'id' => $entity->id(),
+            'is_active' => false
+        ]);
     }
 }
