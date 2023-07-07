e97476582af9917596cf55efc7203d3b0617f6b6
marcelorocfer <marceloengecomp@gmail.com>
2023-07-07 10:30:35 -0300

    Testes de integração usecase de criação de gênero

--- tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
+++ tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
@@ -6,30 +6,64 @@ use Tests\TestCase;
 use App\Models\Genre as GenreModel;
 use App\Models\Category as CategoryModel;
 use Core\UseCase\Genre\CreateGenreUseCase;
+use Core\Domain\Exceptions\NotFoundException;
 use App\Repositories\Transactions\DBTransaction;
 use Core\UseCase\DTO\Genre\Create\GenreCreateInputDTO;
 use App\Repositories\Eloquent\{CategoryRepository, GenreRepository};
 
 class CreateGenreUseCaseTest extends TestCase
 {
-    public function test_insert()
+    public function testInsert()
     {
         $repository = new GenreRepository(new GenreModel());
         $repositoryCategory = new CategoryRepository(new CategoryModel());
+
         $useCase = new CreateGenreUseCase(
             $repository,
             new DBTransaction(),
             $repositoryCategory
         );
 
-        $response = $useCase->execute(
+        $categories = CategoryModel::factory()->count(10)->create();
+        $categoriesIds = $categories->pluck('id')->toArray();
+
+        $useCase->execute(
             new GenreCreateInputDTO(
-                name: 'test'
+                name: 'test',
+                categoriesId: $categoriesIds
             )
         );
 
         $this->assertDatabaseHas('genres', [
             'name' => 'test'
-        ]);
+        ]); 
+
+        $this->assertDatabaseCount('category_genre', 10);
+    }
+
+    public function testExceptionInsertGenreWithCategoriesIdsInvalid() 
+    {
+        $this->expectException(NotFoundException::class);
+
+        $repository = new GenreRepository(new GenreModel());
+        $repositoryCategory = new CategoryRepository(new CategoryModel());
+
+        $useCase = new CreateGenreUseCase(
+            $repository,
+            new DBTransaction(),
+            $repositoryCategory
+        );
+
+        $categories = CategoryModel::factory()->count(10)->create();
+        $categoriesIds = $categories->pluck('id')->toArray();
+
+        array_push($categoriesIds, 'fake_id');
+
+        $useCase->execute(
+            new GenreCreateInputDTO(
+                name: 'test',
+                categoriesId: $categoriesIds
+            )
+        );
     }
 }
