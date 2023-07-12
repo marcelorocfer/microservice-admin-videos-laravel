4fe8150e472e72987e4d2c3acae6a2f3cb8e47ee
marcelorocfer <marceloengecomp@gmail.com>
2023-07-12 11:02:15 -0300

    Aumentando a cobertura de testes e2e na criação de gêneros

--- tests/Feature/Api/GenreApiTest.php
+++ tests/Feature/Api/GenreApiTest.php
@@ -27,6 +27,17 @@ class GenreApiTest extends TestCase
 
         $response->assertStatus(Response::HTTP_OK);
         $response->assertJsonCount(15, 'data');
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from',
+            ]
+        ]);
     }
 
     public function test_store()
@@ -40,5 +51,31 @@ class GenreApiTest extends TestCase
         ]);
 
         $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'is_active'
+            ]
+        ]);
+    }
+
+    public function test_validation_store()
+    {
+        $categories = CategoryModel::factory()->count(2)->create();
+
+        $payload = [
+            'name' => '',
+            'is_active' => true,
+            'categories_ids' => $categories->pluck('id')->toArray()
+        ];
+
+        $response = $this->postJson($this->endpoint, $payload);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => ['name']
+        ]);
     }
 }
