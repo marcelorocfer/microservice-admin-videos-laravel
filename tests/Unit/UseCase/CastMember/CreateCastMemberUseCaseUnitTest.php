b50cf33a3b0511db340e0aaa1a81e40d3e429160
marcelorocfer <marceloengecomp@gmail.com>
2023-07-14 10:35:00 -0300

    Mais testes no usecase de criacão de castmember

--- tests/Unit/UseCase/CastMember/CreateCastMemberUseCaseUnitTest.php
+++ tests/Unit/UseCase/CastMember/CreateCastMemberUseCaseUnitTest.php
@@ -4,9 +4,10 @@ namespace Tests\Unit\UseCase\CastMember;
 
 use Mockery;
 use stdClass;
+use Ramsey\Uuid\Uuid;
 use PHPUnit\Framework\TestCase;
-use Core\Domain\Entity\CastMember as EntityCastMember;
 use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Entity\CastMember as EntityCastMember;
 use Core\UseCase\CastMember\CreateCastMemberUseCase;
 use Core\Domain\Repository\CastMemberRepositoryInterface;
 use Core\UseCase\DTO\CastMember\Create\{
@@ -20,9 +21,10 @@ class CreateCastMemberUseCaseUnitTest extends TestCase
     {
         // arrange
         $mockEntity = Mockery::mock(EntityCastMember::class, ['name', CastMemberType::ACTOR]);
+        $mockEntity->shouldReceive('id');
+        $mockEntity->shouldReceive('created_at')->andReturn(date('Y-m-d H:i:s'));
+
         $mockRepository = Mockery::mock(stdClass::class, CastMemberRepositoryInterface::class);
-        $mockRepository->shouldReceive('id');
-        $mockRepository->shouldReceive('created_at')->andReturn(date('Y-m-d H:i:s'));
         $mockRepository->shouldReceive('insert')
                             ->once()
                             ->andReturn($mockEntity);
@@ -36,7 +38,11 @@ class CreateCastMemberUseCaseUnitTest extends TestCase
         $response = $useCase->execute($mockDTO);
 
         // assert
-        $this->assertInstanceOf(CastMemberCreateOutputDTO::class, $response);
+        $this->assertInstanceOf(CastMemberCreateOutputDTO::class, $response); 
+        $this->assertNotEmpty($response->id);
+        $this->assertEquals('name', $response->name);
+        $this->assertEquals(1, $response->type);
+        $this->assertNotEmpty($response->created_at);
 
         Mockery::close();
     }
