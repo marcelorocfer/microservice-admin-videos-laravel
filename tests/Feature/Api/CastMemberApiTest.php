ec89e3a5b3d3632b147260e1273c696d53d33387
marcelorocfer <marceloengecomp@gmail.com>
2023-07-21 09:03:47 -0300

    Testes e2e atualizar cast member

--- tests/Feature/Api/CastMemberApiTest.php
+++ tests/Feature/Api/CastMemberApiTest.php
@@ -116,4 +116,48 @@ class CastMemberApiTest extends TestCase
             'name' => 'test'
         ]);
     }
+
+    public function test_update_not_found()
+    {
+        $response = $this->putJson("$this->endpoint/fake_id", [
+            'name' => 'test',
+            'type' => 1,
+        ]);
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_update_validations()
+    {
+        $castMember = CastMember::factory()->create();
+
+        $response = $this->putJson("$this->endpoint/{$castMember->id}", []);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name',
+            ]
+        ]);
+    }
+
+    public function test_update()
+    {
+        $castMember = CastMember::factory()->create();
+
+        $response = $this->putJson("$this->endpoint/{$castMember->id}", [
+            'name' => 'New name'
+        ]);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'type',
+                'created_at',
+            ]
+        ]);
+        $this->assertDatabaseHas('cast_members', [
+            'name' => 'New name'
+        ]);
+    }
 }
