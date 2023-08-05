301263f85ec2d1e60b6a34cc9b00a6aa7715052b
marcelorocfer <marceloengecomp@gmail.com>
2023-08-04 21:32:48 -0300

    Criar BaseVideoUseCase

---
+++ src/Core/UseCase/Video/BaseVideoUseCase.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Core\UseCase\Video;
+
+use Core\Domain\Enum\MediaStatus;
+use Core\Domain\Events\VideoCreatedEvent;
+use Core\UseCase\Video\Builder\BuilderVideo;
+use Core\Domain\Exceptions\NotFoundException;
+use Core\UseCase\Interfaces\TransactionInterface;
+use Core\UseCase\Interfaces\FileStorageInterface;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
+
+abstract class BaseVideoUseCase
+{
+    protected BuilderVideo $builder;
+
+    public function __construct(
+        protected VideoRepositoryInterface $repository,
+        protected TransactionInterface $transaction,
+        protected FileStorageInterface $storage,
+        protected VideoEventManagerInterface $eventManager,
+
+        protected CategoryRepositoryInterface $repositoryCategory,
+        protected GenreRepositoryInterface $repositoryGenre,
+        protected CastMemberRepositoryInterface $repositoryCastMember,
+    ) {
+        $this->builder = new BuilderVideo;
+    }
+
+    protected function storageFiles(object $input): void
+    {
+        $entity = $this->builder->getEntity();
+        $path = $entity->id();
+
+        if ($pathVideoFile = $this->storageFile($path, $input->videoFile)) {
+            $this->builder->addMediaVideo($pathVideoFile, MediaStatus::PROCESSING);
+            $this->eventManager->dispatch(new VideoCreatedEvent($entity));
+        }
+
+        if ($pathBannerFile = $this->storageFile($path, $input->bannerFile)) {
+            $this->builder->addTrailer($pathBannerFile);
+        }
+
+        if ($pathThumbFile = $this->storageFile($path, $input->bannerFile)) {
+            $this->builder->addThumb($pathThumbFile);
+        }
+
+        if ($pathThumbHalfFile = $this->storageFile($path, $input->thumbHalf)) {
+            $this->builder->addThumbHalf($pathThumbHalfFile);
+        }
+
+        if ($pathBannerFile = $this->storageFile($path, $input->bannerFile)) {
+            $this->builder->addBanner($pathBannerFile);
+        }
+    }
+
+    protected function storageFile(string $path, ?array $media = null): ?string
+    {
+        if ($media) {
+            return $this->storage->store(
+                path: $path,
+                file: $media,
+            );
+        }
+
+        return null;
+    }
+
+    protected function validateAllIds(object $input)
+    {
+        $this->validateIds(
+            ids: $input->categories,
+            repository: $this->repositoryCategory,
+            singularLabel: 'Category',
+            pluralLabel: 'Categories',
+        );
+
+        $this->validateIds(
+            ids: $input->genres,
+            repository: $this->repositoryGenre,
+            singularLabel: 'Genre',
+        );
+
+        $this->validateIds(
+            ids: $input->castMembers,
+            repository: $this->repositoryCastMember,
+            singularLabel: 'Cast Member',
+        );
+    }
+
+    protected function validateIds(array $ids = [], $repository, string $singularLabel, ?string $pluralLabel = null)
+    {
+        $idsDB = $repository->getIdsListIds($ids);
+
+        $arrayDiff = array_diff($ids, $idsDB);
+
+        if (count($arrayDiff)) {
+            $msg = sprintf(
+                '%s %s not found',
+                count($arrayDiff) > 1 ? $pluralLabel ?? $singularLabel . 's' : $singularLabel,
+                implode(', ', $arrayDiff)
+            );
+
+            throw new NotFoundException($msg);
+        }
+    }
+}
--- src/Core/UseCase/Video/Create/CreateVideoUseCase.php
+++ src/Core/UseCase/Video/Create/CreateVideoUseCase.php
@@ -3,39 +3,14 @@
 namespace Core\UseCase\Video\Create;
 
 use Throwable;
-use Core\Domain\Enum\MediaStatus;
-use Core\Domain\ValueObject\Image;
-use Core\Domain\Entity\Video as Entity;
-use Core\Domain\Events\VideoCreatedEvent;
-use Core\UseCase\Video\Builder\BuilderVideo;
-use Core\Domain\Exceptions\NotFoundException;
-use Core\UseCase\Interfaces\FileStorageInterface;
-use Core\UseCase\Interfaces\TransactionInterface;
-use Core\Domain\Repository\GenreRepositoryInterface;
-use Core\Domain\Repository\VideoRepositoryInterface;
-use Core\UseCase\Video\Create\DTO\CreateInputVideoDTO;
-use Core\Domain\Repository\CategoryRepositoryInterface;
-use Core\UseCase\Video\Create\DTO\CreateOutputVideoDTO;
-use Core\Domain\Repository\CastMemberRepositoryInterface;
-use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
+use Core\UseCase\Video\BaseVideoUseCase;
+use Core\UseCase\Video\Create\DTO\{
+    CreateInputVideoDTO,
+    CreateOutputVideoDTO
+};
 
-class CreateVideoUseCase
+class CreateVideoUseCase extends BaseVideoUseCase
 {
-    protected BuilderVideo $builder;
-
-    public function __construct(
-        protected VideoRepositoryInterface $repository,
-        protected TransactionInterface $transaction,
-        protected FileStorageInterface $storage,
-        protected VideoEventManagerInterface $eventManager,
-
-        protected CategoryRepositoryInterface $repositoryCategory,
-        protected GenreRepositoryInterface $repositoryGenre,
-        protected CastMemberRepositoryInterface $repositoryCastMember,
-    ) {
-        $this->builder = new BuilderVideo;
-    }
-
     public function exec(CreateInputVideoDTO $input): CreateOutputVideoDTO
     {
         $this->validateAllIds($input);
@@ -55,118 +30,6 @@ class CreateVideoUseCase
             $this->transaction->rollback();
             // if (isset($pathMedia)) $this->storage->delete($pathMedia);
             throw $th;
-        }
-
-    }
-
-    protected function storageFiles(object $input): void
-    {
-        $path = $this->builder->getEntity()->id();
-
-        if ($pathVideoFile = $this->storageFile($path, $input->videoFile)) {
-            $this->builder->addMediaVideo($pathVideoFile, MediaStatus::PROCESSING);
-            $this->eventManager->dispatch(new VideoCreatedEvent($this->entity));
-        }
-
-        if ($pathBannerFile = $this->storageFile($path, $input->bannerFile)) {
-            $this->builder->addTrailer($pathBannerFile);
-        }
-
-        if ($pathThumbFile = $this->storageFile($path, $input->bannerFile)) {
-            $this->builder->addThumb($pathThumbFile);
-        }
-
-        if ($pathThumbHalfFile = $this->storageFile($path, $input->thumbHalf)) {
-            $this->builder->addThumbHalf($pathThumbHalfFile);
-        }
-
-        if ($pathBannerFile = $this->storageFile($path, $input->bannerFile)) {
-            $this->builder->addBanner($pathBannerFile);
-        }
-    }
-
-    private function storageFile(string $path, ?array $media = null): ?string
-    {
-        if ($media) {
-            return $this->storage->store(
-                path: $path,
-                file: $media,
-            );
-        }
-
-        return null;
-    }
-
-    protected function validateAllIds(object $input)
-    {
-        $this->validateIds(
-            ids: $input->categories,
-            repository: $this->repositoryCategory,
-            singularLabel: 'Category',
-            pluralLabel: 'Categories',
-        );
-
-        $this->validateIds(
-            ids: $input->genres,
-            repository: $this->repositoryGenre,
-            singularLabel: 'Genre',
-        );
-
-        $this->validateIds(
-            ids: $input->castMembers,
-            repository: $this->repositoryCastMember,
-            singularLabel: 'Cast Member',
-        );
-    }
-
-    private function validateIds(array $ids = [], $repository, string $singularLabel, ?string $pluralLabel = null)
-    {
-        $idsDB = $repository->getIdsListIds($ids);
-
-        $arrayDiff = array_diff($ids, $idsDB);
-
-        if (count($arrayDiff)) {
-            $msg = sprintf(
-                '%s %s not found',
-                count($arrayDiff) > 1 ? $pluralLabel ?? $singularLabel . 's' : $singularLabel,
-                implode(', ', $arrayDiff)
-            );
-
-            throw new NotFoundException($msg);
-        }
-    }
-
-    private function validateGenresId(array $genresId = [])
-    {
-        $genresDB = $this->repositoryGenre->getIdsListIds($genresId);
-
-        $arrayDiff = array_diff($genresId, $genresDB);
-
-        if (count($arrayDiff)) {
-            $msg = sprintf(
-                '%s %s not found',
-                count($arrayDiff) > 1 ? 'Genres' : 'Genre',
-                implode(', ', $arrayDiff)
-            );
-
-            throw new NotFoundException($msg);
-        }
-    }
-
-    private function validateCastMembersId(array $castMembersId = [])
-    {
-        $castMembersDB = $this->repositoryCastMember->getIdsListIds($castMembersId);
-
-        $arrayDiff = array_diff($castMembersId, $castMembersDB);
-
-        if (count($arrayDiff)) {
-            $msg = sprintf(
-                '%s %s not found',
-                count($arrayDiff) > 1 ? 'CastMembers' : 'CastMember',
-                implode(', ', $arrayDiff)
-            );
-
-            throw new NotFoundException($msg);
         }
     }
 
