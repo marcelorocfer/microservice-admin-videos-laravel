9dbf1e5abe21dd2ce9e2ec4ed5e53be0c6a75711
marcelorocfer <marceloengecomp@gmail.com>
2023-08-09 11:08:40 -0300

    Criar o caso de uso para atualizar um vídeo

---
+++ src/Core/UseCase/Video/Update/DTO/UpdateInputVideoDTO.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\UseCase\Video\Update\DTO;
+
+class UpdateInputVideoDTO
+{
+    public function __construct() {}
+}
---
+++ src/Core/UseCase/Video/Update/DTO/UpdateOutputVideoDTO.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\UseCase\Video\Update\DTO;
+
+class UpdateOutputVideoDTO
+{
+    public function __construct() {}
+}
