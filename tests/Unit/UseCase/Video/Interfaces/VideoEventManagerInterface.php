6d69c4f28498b1022910abc34d83f3cf9cc3db01
marcelorocfer <marceloengecomp@gmail.com>
2023-08-10 09:01:43 -0300

    Criar VideoEventManagerInterface

---
+++ tests/Unit/UseCase/Video/Interfaces/VideoEventManagerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Unit\UseCase\Video\Interfaces;
+
+use Core\UseCase\Interfaces\EventManagerInterface;
+
+interface VideoEventManagerInterface extends EventManagerInterface
+{
+}
