52b1dd98549058a0f5b34e3bd63a105faaeb58b3
marcelorocfer <marceloengecomp@gmail.com>
2023-08-10 09:34:02 -0300

    Definindo o comportamento do caso de uso de listagem do vídeo com tdd

--- src/Core/UseCase/Video/List/ListVideoUseCase.php
+++ src/Core/UseCase/Video/List/ListVideoUseCase.php
@@ -3,10 +3,17 @@
 namespace Core\UseCase\Video\List;
 
 use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\List\DTO\ListInputVideoUseCase;
+use Core\UseCase\Video\List\DTO\ListOutputVideoUseCase;
 
 class ListVideoUseCase
 {
     public function __construct(
         private VideoRepositoryInterface $repository
     ) {}
+
+    public function exec(ListInputVideoUseCase $input): ListOutputVideoUseCase
+    {
+
+    }
 }
--- tests/Unit/UseCase/Video/ListVideoUseCaseUnitTest.php
+++ tests/Unit/UseCase/Video/ListVideoUseCaseUnitTest.php
@@ -5,18 +5,32 @@ namespace Tests\Unit\UseCase\Video;
 use Mockery;
 use stdClass;
 use PHPUnit\Framework\TestCase;
+use Core\Domain\ValueObject\Uuid;
 use Core\UseCase\Video\List\ListVideoUseCase;
 use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\List\DTO\ListInputVideoUseCase;
+use Core\UseCase\Video\List\DTO\ListOutputVideoUseCase;
 
 class ListVideoUseCaseUnitTest extends TestCase
 {
     public function test_list()
     {
+        $uuid = Uuid::random();
+
         $useCase = new ListVideoUseCase(
             repository: $this->mockRepository()
         );
 
-        $this->assertTrue(true);
+        $response = $useCase->exec(
+            input: $this->mockInputDTO($uuid)
+        );
+
+        $this->assertInstanceOf(ListInputVideoUseCase::class, $response);
+    }
+
+    private function mockInputDTO(string $id)
+    {
+        return Mockery::mock(ListOutputVideoUseCase::class, [$id]);
     }
 
     private function mockRepository()
