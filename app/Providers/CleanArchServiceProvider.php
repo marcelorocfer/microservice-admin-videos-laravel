dca16727be94ea0c3c1a21ed6549ed17186ba330
marcelorocfer <marceloengecomp@gmail.com>
2023-08-15 15:46:00 -0300

    Inversão de dependência no laravel

--- app/Providers/CleanArchServiceProvider.php
+++ app/Providers/CleanArchServiceProvider.php
@@ -2,15 +2,21 @@
 
 namespace App\Providers;
 
+use App\Events\VideoCreatedEvent;
+use App\Services\Storage\FileStorage;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Eloquent\GenreRepository;
 use App\Repositories\Transactions\DBTransaction;
 use App\Repositories\Eloquent\CategoryRepository;
+use Core\UseCase\Interfaces\FileStorageInterface;
 use Core\UseCase\Interfaces\TransactionInterface;
 use App\Repositories\Eloquent\CastMemberRepository;
 use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use App\Repositories\Eloquent\VideoEloquentRepository;
 use Core\Domain\Repository\CategoryRepositoryInterface;
 use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
 
 class CleanArchServiceProvider extends ServiceProvider
 {
@@ -21,17 +27,16 @@ class CleanArchServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->bindRepositories();
+
         $this->app->singleton(
-            CategoryRepositoryInterface::class,
-            CategoryRepository::class
+            FileStorageInterface::class,
+            FileStorage::class
         );
+
         $this->app->singleton(
-            GenreRepositoryInterface::class,
-            GenreRepository::class,
-        );
-        $this->app->singleton(
-            CastMemberRepositoryInterface::class,
-            CastMemberRepository::class,
+            VideoEventManagerInterface::class,
+            VideoCreatedEvent::class
         );
 
         /**
@@ -52,5 +57,28 @@ class CleanArchServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    private function bindRepositories()
+    {
+        /**
+         * Repositories
+         */
+        $this->app->singleton(
+            CategoryRepositoryInterface::class,
+            CategoryRepository::class
+        );
+        $this->app->singleton(
+            GenreRepositoryInterface::class,
+            GenreRepository::class,
+        );
+        $this->app->singleton(
+            CastMemberRepositoryInterface::class,
+            CastMemberRepository::class,
+        );
+        $this->app->singleton(
+            VideoRepositoryInterface::class,
+            VideoEloquentRepository::class,
+        );
     }
 }
