a66661c260abd0b4ff6c35d1b12cad2df2946f28
marcelorocfer <marceloengecomp@gmail.com>
2023-08-18 09:57:55 -0300

    Using closures no laravel para gerar uuid

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Media extends Model
 {
@@ -25,6 +26,18 @@ class Media extends Model
         'is_active' => 'boolean',
         'deleted_at' => 'datetime',
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
 
     public function video()
     {
