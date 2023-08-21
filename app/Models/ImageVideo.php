910c07b367fba9073d7ed1a82ce88c74090883e0
marcelorocfer <marceloengecomp@gmail.com>
2023-08-21 09:47:21 -0300

    Teste unitário do model de imagem do video

--- app/Models/ImageVideo.php
+++ app/Models/ImageVideo.php
@@ -2,15 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ImageVideo extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
 
     protected $table = 'images_video';
-    
+
+    protected $fillable = [
+        'path',
+        'type',
+    ];
+
+    public $incrementing = false;
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean',
+        'deleted_at' => 'datetime',
+    ];
+
     public function video()
     {
         return $this->belongsTo(Video::class);
