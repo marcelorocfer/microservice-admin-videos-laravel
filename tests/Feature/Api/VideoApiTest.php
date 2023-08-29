a934b173be39f7bf2595af0317a46beb0d6ab8d2
marcelorocfer <marceloengecomp@gmail.com>
2023-08-29 10:47:14 -0300

    Testes e2e e implementar paginação

--- tests/Feature/Api/VideoApiTest.php
+++ tests/Feature/Api/VideoApiTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Api;
 
 use Tests\TestCase;
+use App\Models\Video;
 use Illuminate\Http\Response;
 
 class VideoApiTest extends TestCase
@@ -14,5 +15,15 @@ class VideoApiTest extends TestCase
         $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function testPagination()
+    {
+        $videos = Video::factory()->count(20)->create();
+
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(15, 'data');
     }
 }
