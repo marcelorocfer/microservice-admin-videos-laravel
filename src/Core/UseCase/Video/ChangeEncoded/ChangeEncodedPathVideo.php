3874743e5e08df355bcff4300ab45cd3b8b70228
marcelorocfer <marceloengecomp@gmail.com>
2023-09-04 21:51:24 -0300

    Spies no teste unitário

--- src/Core/UseCase/Video/ChangeEncoded/ChangeEncodedPathVideo.php
+++ src/Core/UseCase/Video/ChangeEncoded/ChangeEncodedPathVideo.php
@@ -2,6 +2,7 @@
 
 namespace Core\UseCase\Video\ChangeEncoded;
 
+use Core\UseCase\Video\DTO\ChangeEncodedVideoDTO;
 use Core\Domain\Repository\VideoRepositoryInterface;
 
 class ChangeEncodedPathVideo
@@ -9,4 +10,9 @@ class ChangeEncodedPathVideo
     public function __construct(
         protected VideoRepositoryInterface $repository
     ) {}
+
+    public function exec(ChangeEncodedVideoDTO $input): void
+    {
+        $this->repository->findById($input->id);
+    }
 }
--- tests/Unit/UseCase/Video/ChangeEncodedPathVideoUnitTest.php
+++ tests/Unit/UseCase/Video/ChangeEncodedPathVideoUnitTest.php
@@ -4,7 +4,10 @@ namespace Tests\Unit\UseCase\Video;
 
 use Mockery;
 use stdClass;
+use Core\Domain\Enum\Rating;
 use PHPUnit\Framework\TestCase;
+use Core\Domain\Entity\Video as Entity;
+use Core\UseCase\Video\DTO\ChangeEncodedVideoDTO;
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\UseCase\Video\ChangeEncoded\ChangeEncodedPathVideo;
 
@@ -12,10 +15,35 @@ class ChangeEncodedPathVideoUnitTest extends TestCase
 {
     public function testSpies()
     {
+        $input = new ChangeEncodedVideoDTO(
+            id: 'id-video',
+            encodedPath: 'path/video_encoded.ext',
+        );
+
         $mockRepository = Mockery::mock(stdClass::class, VideoRepositoryInterface::class);
+        $mockRepository->shouldReceive('findById')
+                        ->times(1)
+                        ->with($input->id)
+                        ->andReturn($this->getEntity());
 
         $useCase = new ChangeEncodedPathVideo(
             repository: $mockRepository
+        );
+
+        $useCase->exec(input: $input);
+
+        Mockery::close();
+    }
+
+    private function getEntity(): Entity
+    {
+        return new Entity(
+            title: 'title',
+            description: 'description',
+            yearLaunched: 2026,
+            duration: 1,
+            opened: true,
+            rating: Rating::L,
         );
     }
 }
